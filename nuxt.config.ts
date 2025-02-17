80db0ba85f375f686d7f6a3f11d79fb761e8036d
prog-nori <norinorimcz@gmail.com>
2025-02-17 22:17:26 +0900

    The first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+import { defineNuxtConfig } from 'nuxt/config';
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+});
